75f321b7d469f95a21b30e2b2c4271f823c7255e
unknown <perezoscar360@gmail.com>
2022-12-07 22:24:54 -0600

    finished experience and services .js files and styles them using pure css

--- react-portfolio-website/src/components/experience/Experience.js
+++ react-portfolio-website/src/components/experience/Experience.js
@@ -1,9 +1,124 @@
 import React from 'react'
 import './experience.css'
+import { AiOutlineCheckCircle } from 'react-icons/ai'
 
 const Experience = () => {
   return (
-    <div>Experience</div>
+    <section id='experience'>
+      <h5>Skills of mine</h5>
+      <h2>My Experience</h2>
+
+      <div className='container experience__container'>
+        <div className='experience__frontend'>
+          <h3>Frontend Development</h3>
+          <div className='experience__content'>
+
+            <article className='experience__details'>
+              <AiOutlineCheckCircle className='experience__details-icon'/>
+              <div>
+                <h4>HTML</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+
+            <article className='experience__details'>
+              <AiOutlineCheckCircle className='experience__details-icon'/>
+              <div>
+                <h4>CSS</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+
+            <article className='experience__details'>
+              <AiOutlineCheckCircle className='experience__details-icon'/>
+              <div>
+                <h4>JavaScript</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+
+            <article className='experience__details'>
+              <AiOutlineCheckCircle className='experience__details-icon'/>
+              <div>
+                <h4>Bootstrap</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+
+            <article className='experience__details'>
+              <AiOutlineCheckCircle className='experience__details-icon'/>
+              <div>
+                <h4>Materialize</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+
+            <article className='experience__details'>
+              <AiOutlineCheckCircle className='experience__details-icon'/>
+              <div>
+                <h4>React</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+
+          </div>
+        </div>
+        <div className='experience__backend'>
+          <h3>Backend Development</h3>
+          <div className='experience__content'>
+
+            <article className='experience__details'>
+              <AiOutlineCheckCircle className='experience__details-icon'/>
+              <div>
+                <h4>Node JS</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+
+            <article className='experience__details'>
+              <AiOutlineCheckCircle className='experience__details-icon'/>
+              <div>
+                <h4>OOP</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+
+            <article className='experience__details'>
+              <AiOutlineCheckCircle className='experience__details-icon'/>
+              <div>
+                <h4>Express</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+
+            <article className='experience__details'>
+              <AiOutlineCheckCircle className='experience__details-icon'/>
+              <div>
+                <h4>MySQL</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+
+            <article className='experience__details'>
+              <AiOutlineCheckCircle className='experience__details-icon'/>
+              <div>
+                <h4>MongoDB</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+
+            <article className='experience__details'>
+              <AiOutlineCheckCircle className='experience__details-icon'/>
+              <div>
+                <h4>MVC</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
--- react-portfolio-website/src/components/services/Services.js
+++ react-portfolio-website/src/components/services/Services.js
@@ -1,9 +1,115 @@
 import React from 'react'
 import './services.css'
+import {BsCardChecklist} from 'react-icons/bs'
 
 const Services = () => {
   return (
-    <div>Services</div>
+    <section id='services'>
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
+
+      <div className='container services_container'>
+        <article className='service'>
+          <div className='service_head'>
+            <h3>UI/UX Design</h3>
+          </div>
+
+          <ul className='service_list'>
+            <li>
+              <BsCardChecklist className='service_list-icon' />
+              <p>JavaScript ES5 - ES6, CSS3, HTML5</p>
+            </li>
+            <li>
+              <BsCardChecklist className='service_list-icon' />
+              <p>JavaScript ES5 - ES6, CSS3, HTML5</p>
+            </li>
+            <li>
+              <BsCardChecklist className='service_list-icon' />
+              <p>JavaScript ES5 - ES6, CSS3, HTML5</p>
+            </li>
+            <li>
+              <BsCardChecklist className='service_list-icon' />
+              <p>JavaScript ES5 - ES6, CSS3, HTML5</p>
+            </li>
+            <li>
+              <BsCardChecklist className='service_list-icon' />
+              <p>JavaScript ES5 - ES6, CSS3, HTML5</p>
+            </li>
+            <li>
+              <BsCardChecklist className='service_list-icon' />
+              <p>JavaScript ES5 - ES6, CSS3, HTML5</p>
+            </li>
+          </ul>
+        </article>
+
+        <article className='service'>
+          <div className='service_head'>
+            <h3>Web Development</h3>
+          </div>
+
+          <ul className='service_list'>
+            <li>
+              <BsCardChecklist className='service_list-icon' />
+              <p>JavaScript ES5 - ES6, CSS3, HTML5</p>
+            </li>
+            <li>
+              <BsCardChecklist className='service_list-icon' />
+              <p>JavaScript ES5 - ES6, CSS3, HTML5</p>
+            </li>
+            <li>
+              <BsCardChecklist className='service_list-icon' />
+              <p>JavaScript ES5 - ES6, CSS3, HTML5</p>
+            </li>
+            <li>
+              <BsCardChecklist className='service_list-icon' />
+              <p>JavaScript ES5 - ES6, CSS3, HTML5</p>
+            </li>
+            <li>
+              <BsCardChecklist className='service_list-icon' />
+              <p>JavaScript ES5 - ES6, CSS3, HTML5</p>
+            </li>
+            <li>
+              <BsCardChecklist className='service_list-icon' />
+              <p>JavaScript ES5 - ES6, CSS3, HTML5</p>
+            </li>
+          </ul>
+        </article>
+
+        <article className='service'>
+          <div className='service_head'>
+            <h3>Content Creation</h3>
+          </div>
+
+          <ul className='service_list'>
+            <li>
+              <BsCardChecklist className='service_list-icon' />
+              <p>JavaScript ES5 - ES6, CSS3, HTML5</p>
+            </li>
+            <li>
+              <BsCardChecklist className='service_list-icon' />
+              <p>JavaScript ES5 - ES6, CSS3, HTML5</p>
+            </li>
+            <li>
+              <BsCardChecklist className='service_list-icon' />
+              <p>JavaScript ES5 - ES6, CSS3, HTML5</p>
+            </li>
+            <li>
+              <BsCardChecklist className='service_list-icon' />
+              <p>JavaScript ES5 - ES6, CSS3, HTML5</p>
+            </li>
+            <li>
+              <BsCardChecklist className='service_list-icon' />
+              <p>JavaScript ES5 - ES6, CSS3, HTML5</p>
+            </li>
+            <li>
+              <BsCardChecklist className='service_list-icon' />
+              <p>JavaScript ES5 - ES6, CSS3, HTML5</p>
+            </li>
+          </ul>
+        </article>
+
+      </div>
+    </section>
   )
 }
 
