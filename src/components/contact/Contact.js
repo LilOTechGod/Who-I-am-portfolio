8670d1ed85d3876bd0b79648c6daa02948cad943
unknown <perezoscar360@gmail.com>
2022-12-09 00:48:04 -0600

    finished contact component and started writing javascript for the form to send to my email

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import './contact.css'
+import {GrMailOption} from 'react-icons/gr'
+import {AiOutlineMessage} from 'react-icons/ai'
+import {BsGithub} from 'react-icons/bs'
 
 const Contact = () => {
   return (
@@ -8,14 +11,33 @@ const Contact = () => {
       <h2>Contact Me</h2>
 
       <div className='container contact_container'>
-        <div className='cpntact_options'>
+        <div className='contact_options'>
           <article className='contact_option'>
+            <GrMailOption className='contact_option-icon'/>
             <h4>Email</h4>
             <h5>perezoscar360@gmail.com</h5>
-            <a href='perezoscar360@gmail.com'>Send A Message</a>
+            <a href='mailto:perezoscar360@gmail.com' target='_blank'>Send An Email </a>
+          </article>
+          <article className='contact_option'>
+            <AiOutlineMessage className='contact_option-icon'/>
+            <h4>Messenger</h4>
+            <h5>LinkedIn</h5>
+            <a href='https://www.linkedin.com/in/oscar-perez~hernandez/' target='_blank'>Send A Message</a>
+          </article>
+          <article className='contact_option'>
+            <BsGithub className='contact_option-icon'/>
+            <h4>Github</h4>
+            {/*  */}
+            <a href='https://github.com/LilOTechGod' target='_blank'>View GitHub</a>
           </article>
         </div>
-        <form action=''></form>
+        {/* End of contact options */}
+        <form action=''>
+          <input type='text' name='name' placeholder='Your Full Name' required />
+          <input type='email' name='email' placeholder='Your Email' required />
+          <textarea name='message' rows='7' placeholder='Your Message' required ></textarea>
+          <button type='submit' className='btn btn-primary'>Send</button>
+        </form>
       </div>
     </section>
   )
