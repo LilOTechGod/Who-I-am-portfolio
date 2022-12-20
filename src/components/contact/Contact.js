fa82f3234232d5faa8d138e4e25430e9ce2bbf2c
unknown <perezoscar360@gmail.com>
2022-12-20 15:08:34 -0600

    added styling to footer and updated all media queries

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -6,7 +6,7 @@ import {BsGithub} from 'react-icons/bs'
 import { useRef } from 'react';
 import emailjs from 'emailjs-com'
 import {FiSend} from 'react-icons/fi'
-import {AiOutlineSend} from 'react-icons/ai'
+import {CiViewTimeline} from 'react-icons/ci'
 
 const Contact = () => {
   const form = useRef();
@@ -36,16 +36,16 @@ const Contact = () => {
             <AiOutlineMessage className='contact_option-icon'/>
             <h4>Messenger:</h4>
             <h5>LinkedIn</h5>
-            <a href='https://www.linkedin.com/in/oscar-perez~hernandez/' target='_blank'>Send A Message</a> <AiOutlineSend/>
+            <a href='https://www.linkedin.com/in/oscar-perez~hernandez/' target='_blank'>Send A Message</a><FiSend/>
           </article>
           <article className='contact_option'>
             <BsGithub className='contact_option-icon'/>
             <h4>Github:</h4>
-            {/*  */}
-            <a href='https://github.com/LilOTechGod' target='_blank'>View GitHub</a>
+            <a href='https://github.com/LilOTechGod' target='_blank'>View GitHub</a><CiViewTimeline/>
           </article>
         </div>
         {/* End of contact options */}
+        
         <form ref={form} onSubmit={sendEmail}>
           <input type='text' name='name' placeholder='Your Full Name' required />
           <input type='email' name='email' placeholder='Your Email' required />
